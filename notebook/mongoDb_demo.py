3d1e25e55799b99bb7b0c1ca05208fbe56387cb0
mpsoni289 <mpsoni289@gmail.com>
2025-09-11 23:33:01 +0530

    Add folder structure , logging , exception

---
+++ notebook/mongoDb_demo.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+# import pymongo
+
+# df = pd.read_csv('data.csv')
+# df.head()
+
+# data = df.to_dict(orient='records')
+
+# DB_NAME = "Proj1"
+# COLLECTION_NAME = "Proj1-Data"
+# # CONNECTION_URL = ""
+
+# client = pymongo.MongoClient(CONNECTION_URL)
+# data_base = client[DB_NAME]
+# collection = data_base[COLLECTION_NAME]
+
+# # Uploading data to MongoDB
+# rec = collection.insert_many(data)
+
