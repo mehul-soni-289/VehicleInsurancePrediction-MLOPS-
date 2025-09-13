020e33a84d308d236da430c37e8ea2558b6f2a12
mpsoni289 <mpsoni289@gmail.com>
2025-09-13 18:11:03 +0530

    create folder structure and add data to mongodb

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mehul Soni",
+    author_email="mehulsoni289@gmail.com",
+    packages=find_packages()
+)
